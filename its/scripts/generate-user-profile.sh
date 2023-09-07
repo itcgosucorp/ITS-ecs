bea1f6a0eb02445baa177a29e649aace28b8914a
Duc Nguyen <nhmduc@gmail.com>
2023-09-07 11:05:57 +0700

    feat: generated user-profile schema

--- its/scripts/generate-user-profile.sh
+++ its/scripts/generate-user-profile.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 scripts/generator.py --include its/user-profile/custom/ --subset its/user-profile/subset.yml --out its/user-profile/
+python3 scripts/generator.py --include its/user-profile/fields/custom/ --subset its/user-profile/fields/subset.yml --out its/user-profile/
