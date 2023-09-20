97dd46ee7d8f5194db9dcf7483f02108292e1429
Duc Nguyen <nhmduc@gmail.com>
2023-09-20 10:34:04 +0700

    refactor: index-templates folder

---
+++ its/index-templates/user-profile/scripts/generator.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 scripts/generator.py --force-docs --template-settings-legacy its/index-templates/user-profile/fields/template-legacy.json --template-settings its/index-templates/user-profile/fields/template.json --mapping-settings its/index-templates/user-profile/fields/mapping.json --include its/schemas/ --subset its/index-templates/user-profile/fields/subset.yml --out its/index-templates/user-profile/
