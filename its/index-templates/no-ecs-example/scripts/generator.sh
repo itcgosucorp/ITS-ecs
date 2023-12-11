07cb4a29fd010daf2077de76cbe65a0a3c0d7611
Duc Nguyen <nhmduc@gmail.com>
2023-12-11 11:10:18 +0700

    feat: no ecs example

---
+++ its/index-templates/no-ecs-example/scripts/generator.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 scripts/generator.py --template-settings-legacy its/index-templates/no-ecs-example/fields/template-legacy.json --template-settings its/index-templates/no-ecs-example/fields/template.json --mapping-settings its/index-templates/no-ecs-example/fields/mapping.json --include its/schemas/ --subset its/index-templates/no-ecs-example/fields/subset.yml --out its/index-templates/no-ecs-example/
