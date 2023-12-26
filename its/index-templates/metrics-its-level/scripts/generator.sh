4d0b11df72419a343f738cd8bb7ccc265d12f5cf
Huỳnh Đình Tiến <39383420+tienhuynh17@users.noreply.github.com>
2023-12-26 16:21:47 +0700

    feat: metrics_level index template (#13)

---
+++ its/index-templates/metrics-its-level/scripts/generator.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 scripts/generator.py --template-settings-legacy its/index-templates/metrics-its-level/fields/template-legacy.json --template-settings its/index-templates/metrics-its-level/fields/template.json --mapping-settings its/index-templates/metrics-its-level/fields/mapping.json --include its/schemas/ --subset its/index-templates/metrics-its-level/fields/subset.yml --out its/index-templates/metrics-its-level/
