992b998bb029c80a3816556155fcfe959b4206c0
Huỳnh Đình Tiến <39383420+tienhuynh17@users.noreply.github.com>
2023-12-28 11:53:06 +0700

    Feature/tasklog index template (#14)

---
+++ its/index-templates/task-log/scripts/generator.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 scripts/generator.py --template-settings-legacy its/index-templates/task-log/fields/template-legacy.json --template-settings its/index-templates/task-log/fields/template.json --mapping-settings its/index-templates/task-log/fields/mapping.json --include its/schemas/ --subset its/index-templates/task-log/fields/subset.yml --out its/index-templates/task-log/
