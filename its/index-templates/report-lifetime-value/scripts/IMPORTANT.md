b052fe5eb1538aace18ed479c244309bf66a281a
Duc Nguyen <nhmduc@gmail.com>
2024-11-15 16:53:04 +0700

    feat: report lifetime value

---
+++ its/index-templates/report-lifetime-value/scripts/IMPORTANT.md
@@ -0,0 +1,2 @@
+Sau khi gen thành công.
+- Vào `its/index-templates/report-lifetime-value/generated/elasticsearch/legacy/template.json` để chỉnh lại field `its_report_retention_rate` thành top_level root.
---
+++ its/index-templates/report-lifetime-value/scripts/generator.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 scripts/generator.py --template-settings-legacy its/index-templates/report-lifetime-value/fields/template-legacy.json --template-settings its/index-templates/report-lifetime-value/fields/template.json --mapping-settings its/index-templates/report-lifetime-value/fields/mapping.json --include its/schemas/ --subset its/index-templates/report-lifetime-value/fields/subset.yml --out its/index-templates/report-lifetime-value/
