ae59cb69480ae78de752ee9ed7ea54c6734e3fcc
Duc Nguyen <nhmduc@gmail.com>
2024-11-28 13:50:48 +0700

    feat: onelink template

---
+++ its/index-templates/onelink/scripts/generator.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 scripts/generator.py --force-docs --template-settings-legacy its/index-templates/onelink/fields/template-legacy.json --template-settings its/index-templates/onelink/fields/template.json --mapping-settings its/index-templates/onelink/fields/mapping.json --include its/schemas --subset its/index-templates/onelink/fields/subset.yml --out its/index-templates/onelink/
