706493e5bc3d7be084d3eceeaf8df2dc1696202f
Duc Nguyen <nhmduc@gmail.com>
2024-12-10 12:59:18 +0700

    feat: report pr

---
+++ its/index-templates/report-pr/scripts/IMPORTANT.md
@@ -0,0 +1,2 @@
+Sau khi gen thành công.
+- Vào `its/index-templates/report-pr/generated/elasticsearch/legacy/template.json` để chỉnh lại field `its_report_pr` thành top_level root.
---
+++ its/index-templates/report-pr/scripts/generator.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 scripts/generator.py --template-settings-legacy its/index-templates/report-pr/fields/template-legacy.json --template-settings its/index-templates/report-pr/fields/template.json --mapping-settings its/index-templates/report-pr/fields/mapping.json --include its/schemas/ --subset its/index-templates/report-pr/fields/subset.yml --out its/index-templates/report-pr/
