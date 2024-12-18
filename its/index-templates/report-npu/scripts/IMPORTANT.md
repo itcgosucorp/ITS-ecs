4ade16ce4efd0879dbb05c74585be73c0dc11191
Duc Nguyen <nhmduc@gmail.com>
2024-12-18 17:11:18 +0700

    feat: report npu

---
+++ its/index-templates/report-npu/scripts/IMPORTANT.md
@@ -0,0 +1,2 @@
+Sau khi gen thành công.
+- Vào `its/index-templates/report-npu/generated/elasticsearch/legacy/template.json` để chỉnh lại field `its_report_npu` thành top_level root.
---
+++ its/index-templates/report-npu/scripts/generator.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 scripts/generator.py --template-settings-legacy its/index-templates/report-npu/fields/template-legacy.json --template-settings its/index-templates/report-npu/fields/template.json --mapping-settings its/index-templates/report-npu/fields/mapping.json --include its/schemas/ --subset its/index-templates/report-npu/fields/subset.yml --out its/index-templates/report-npu/
