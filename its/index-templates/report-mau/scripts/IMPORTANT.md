2f23ddad81141f35f01faea82b1aad54fe1f4552
Duc Nguyen <nhmduc@gmail.com>
2025-04-15 16:19:26 +0700

    feat: report mau

---
+++ its/index-templates/report-mau/scripts/IMPORTANT.md
@@ -0,0 +1,2 @@
+Sau khi gen thành công.
+- Vào `its/index-templates/report-mau/generated/elasticsearch/legacy/template.json` để chỉnh lại field `its_report_mau` thành top_level root.
---
+++ its/index-templates/report-mau/scripts/generator.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 scripts/generator.py --template-settings-legacy its/index-templates/report-mau/fields/template-legacy.json --template-settings its/index-templates/report-mau/fields/template.json --mapping-settings its/index-templates/report-mau/fields/mapping.json --include its/schemas/ --subset its/index-templates/report-mau/fields/subset.yml --out its/index-templates/report-mau/
