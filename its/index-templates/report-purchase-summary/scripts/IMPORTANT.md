50f4e559ebfdeb0707c7b8c742c6f9087e70011a
Duc Nguyen <nhmduc@gmail.com>
2025-04-22 10:10:53 +0700

    feat: report purchase summary

---
+++ its/index-templates/report-purchase-summary/scripts/IMPORTANT.md
@@ -0,0 +1,2 @@
+Sau khi gen thành công.
+- Vào `its/index-templates/report-purchase-summary/generated/elasticsearch/legacy/template.json` để chỉnh lại field `its_report_purchase_summary` thành top_level root.
---
+++ its/index-templates/report-purchase-summary/scripts/generator.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 scripts/generator.py --template-settings-legacy its/index-templates/report-purchase-summary/fields/template-legacy.json --template-settings its/index-templates/report-purchase-summary/fields/template.json --mapping-settings its/index-templates/report-purchase-summary/fields/mapping.json --include its/schemas/ --subset its/index-templates/report-purchase-summary/fields/subset.yml --out its/index-templates/report-purchase-summary/
